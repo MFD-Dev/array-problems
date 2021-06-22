c4aff5939588f64940116b75e683beec4bc2a558
Michael <Michaelflowersdev@gmail.com>
2021-06-22 10:50:59 -0500

    corrected and completed problem 4

--- arrays-problem4.js
+++ arrays-problem4.js
@@ -4,7 +4,14 @@
 // Write a JavaScript program to remove duplicate items from 
 // an array (consider case sensitivity).
 // Example array: [1,2,2,4,5,4,7,8,7,3,6]
-
+/*
+-- 7 different ways.  
+-- using indexOf() and filter()
+-- create a function with the one param
+-- use the .filter() method to grab value and index. 
+-- use indexOf() to see which index is different from it's indexOf value.
+*** consider case sensitivity?  as if it was a string?
+*/
 let array = [1,2,2,4,5,4,7,8,7,3,6];
 
 function removeDuplicates(data) {
