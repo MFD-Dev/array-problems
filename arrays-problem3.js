4be0abd0f15ca4f4c80bd175570df6daa6601631
Michael <Michaelflowersdev@gmail.com>
2021-06-22 10:39:04 -0500

    corrected and completed problem 3

--- arrays-problem3.js
+++ arrays-problem3.js
@@ -2,8 +2,13 @@
 // Module 3: JavaScript
 // Write a JavaScript program to sort the items of an array.
 // Example array: var myArray = [-3,8,7,6,5,-4,3,2,1];
-
-
+/*
+-- create a function 
+-- use the sort() method
+*/
 var myArray = [-3,8,7,6,5,-4,3,2,1];
 
-console.log(myArray.sort());
+myArray.sort(function(a, b){return a - b});
+
+console.log(myArray)
+// console.log(myArray.sort());
