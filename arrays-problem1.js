37aec85b74e212eac8587f290aa004860e9aca00
Michael <Michaelflowersdev@gmail.com>
2021-07-14 09:33:52 -0500

    update

--- arrays-problem1.js
+++ arrays-problem1.js
@@ -16,11 +16,9 @@ Example: If I pass in an array and the number 2, your function returns the first
 let array = [2, 4, 7, 9]
 
 function myFunction(arr, n) {
-    for (var i = 0; i < array.length; i++) {  // loop through the array.  incrementing by 1.
-          return array.slice(0, 2) //use slice to grab the first 2 indexes
+
+     return arr.slice(0, n) ; //use slice to grab the first 2 indexes
+         
     }
   
-    
-}
-
-console.log(myFunction(array))
+console.log(myFunction(array, 2)); // [2, 4]
--- arrays-problem3.js
+++ arrays-problem3.js
@@ -8,7 +8,10 @@
 */
 var myArray = [-3,8,7,6,5,-4,3,2,1];
 
-myArray.sort(function(a, b){return a - b});
+function myFunction() {
+    myArray.sort(function(a, b){return a - b});
+}
 
-console.log(myArray)
+
+console.log(myFunction())
 // console.log(myArray.sort());
